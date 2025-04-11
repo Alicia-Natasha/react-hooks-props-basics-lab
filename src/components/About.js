665b4b4afc5acba8593ff33add990f31daafb36e
Alicia-Natasha <alicia.natasha@student.moringaschool.com>
2025-04-12 00:42:32 +0300

    react-hooks-props-basics-lab

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,8 @@ function About({ bio, github, linkedin}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{
+        {bio && <p>{bio}</p>}
        <Links github={github} linkedin={linkedin} />
-
-        }</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {<Links/>}
     </div>
